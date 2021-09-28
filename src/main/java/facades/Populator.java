775da09e9623ff2658e4692b40e8188b473617d4
Søren Aagaard Bendtsen <soren@tech-logic.dk>
2021-09-28 13:10:31 +0200

    vi mangler data

--- src/main/java/facades/Populator.java
+++ src/main/java/facades/Populator.java
@@ -6,6 +6,7 @@
 package facades;
 
 import dtos.RenameMeDTO;
+import entities.Hobby;
 import entities.RenameMe;
 import javax.persistence.EntityManagerFactory;
 import utils.EMF_Creator;
@@ -18,9 +19,8 @@ public class Populator {
     public static void populate(){
         EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory();
         FacadeExample fe = FacadeExample.getFacadeExample(emf);
-        fe.create(new RenameMeDTO(new RenameMe("First 1", "Last 1")));
-        fe.create(new RenameMeDTO(new RenameMe("First 2", "Last 2")));
-        fe.create(new RenameMeDTO(new RenameMe("First 3", "Last 3")));
+
+
     }
     
     public static void main(String[] args) {
