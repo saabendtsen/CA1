e426626716f547a35dd9931d996568500b366379
Søren Aagaard Bendtsen <soren@tech-logic.dk>
2021-09-29 10:47:08 +0200

    created facade

---
+++ src/main/java/facades/PersonFacade.java
@@ -0,0 +1,26 @@
+package facades;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+public class PersonFacade {
+
+    private static PersonFacade instance;
+    private static EntityManagerFactory emf;
+
+    //Private Constructor to ensure Singleton
+    private PersonFacade() {}
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+
+    public static PersonFacade getFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new PersonFacade();
+        }
+        return instance;
+    }
+}
