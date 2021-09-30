891793009432792fc9b4fb6c743a6147fdc1e937
Slider <cph-yk31@cphbusiness.dk>
2021-09-30 18:09:55 +0200

    add some points

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -2,73 +2,91 @@ package rest;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import dtos.CityInfoDTO;
+import dtos.HobbyDTO;
 import dtos.PersonDTO;
-import entities.*;
 import facades.PersonFacade;
 import utils.EMF_Creator;
-
 import javax.persistence.EntityManagerFactory;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("/person")
 public class PersonResource {
     private final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
-    private final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private final PersonFacade pf = PersonFacade.getPersonFacade(EMF);
+
 
     @GET
     @Produces("application/json")
-    public String getAllPersons() {
-        List<PersonDTO> personDTOList = pf.getAllPersons();
-        return GSON.toJson(personDTOList);
+    public Response getAllPersons() {
+        return Response.ok(gson.toJson(pf.getAllPersons()), "application/json").build();
     }
 
-/*
     @POST
     @Produces("application/json")
     @Consumes("application/json")
-    public String createPerson(String person) throws Exception {
-        PersonDTO personDTO = GSON.fromJson(person,PersonDTO.class);
-        personDTO = pf.createPerson(personDTO);
-        return GSON.toJson(personDTO);
+    public Response addPerson(String person) throws Exception {
+        PersonDTO dto = gson.fromJson(person, PersonDTO.class);
+        dto = pf.createPerson(dto);
+        return Response.ok(gson.toJson(dto), "application/json").build();
     }
 
-    @GET
     @Path("{id}")
-    @Produces("application/json")
-    public String getSinglePerson(@PathParam("id")long id) throws Exception {
-        PersonDTO personDTO = pf.getSinglePerson(id);
-
-        return GSON.toJson(personDTO);
-    }
-
     @PUT
     @Produces("application/json")
     @Consumes("application/json")
-    public String updatePerson(String person) throws Exception {
-        PersonDTO personDTO = GSON.fromJson(person,PersonDTO.class);
-        personDTO = pf.updatePerson(personDTO);
-        return GSON.toJson(personDTO);
+    public Response editPerson(@PathParam("id") long id, String person) throws Exception {
+        PersonDTO dto = gson.fromJson(person, PersonDTO.class);
+        dto.setId(id);
+        dto = pf.updatePerson(dto);
+        return Response.ok(gson.toJson(dto), "application/json").build();
     }
-
 
     @Path("{id}")
     @DELETE
     @Produces("application/json")
-    public String deletePerson(@PathParam("id")long id) throws Exception {
-        PersonDTO personDTO = pf.deletePerson(id);
-
-        return GSON.toJson(personDTO);
+    @Consumes("application/json")
+    public Response deletePerson(@PathParam("id") long id) throws Exception {
+        PersonDTO dto = pf.deletePerson(id);
+        return Response.ok("{\"status\" : \"removed: " + gson.toJson(dto) + "\"}", "application/json").build();
     }
-/*
+
+    @Path("{id}")
+    @GET
+    @Produces("application/json")
+    public Response getPersonById(@PathParam("id") long id) throws Exception {
+        PersonDTO dto = pf.getSinglePerson(id);
+        return Response.ok(gson.toJson(dto), "application/json").build();
+    }
+
+    @Path("hobby/{hobby}")
+    @GET
+    @Produces("application/json")
+    public Response getAllPersonsWithHobby(@PathParam("hobby") String hobby) throws Exception {
+        HobbyDTO hobbyDTO = gson.fromJson(hobby, HobbyDTO.class);
+        List<PersonDTO> dto = pf.getAllPersonsWithHobby(hobbyDTO);
+        return Response.ok(gson.toJson(dto), "application/json").build();
+    }
+
+    @Path("zipcodes")
+    @GET
+    @Produces("application/json")
+    public Response getAllZipcodes() {
+        return Response.ok(gson.toJson(pf.getAllZipcodes()), "application/json").build();
+    }
+
+    /*
+
+
     @Path("{hobby}")
     @GET
     @Produces("application/json")
-    public String getAllPersonsWithHobby(@PathParam("hobby")String hobby){
-        HobbyDTO hobbyDTO = GSON.fromJson(hobby,HobbyDTO.class);
+    public String getAllPersonsWithHobby(@PathParam("hobby") String hobby) {
+        HobbyDTO hobbyDTO = GSON.fromJson(hobby, HobbyDTO.class);
         List<PersonDTO> personDTOS = pf.getAllPersonsWithHobby(hobbyDTO);
 
         return GSON.toJson(personDTOS);
@@ -78,16 +96,11 @@ public class PersonResource {
     @Path("{city}")
     @GET
     @Produces("application/json")
-    public String getAllPersonInCity(String person){
+    public String getAllPersonInCity(String person) {
         return null;
     }
 
-    @Path("{zipcode}")
-    @GET
-    @Produces("application/json")
-    public Response getAllZipcodes(){
-        return null;
-    }
 
- */
+
+*/
 }
