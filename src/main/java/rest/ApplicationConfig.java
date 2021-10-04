b08dfda2023e164e02d92be360f70520dd41e76f
Søren Aagaard Bendtsen <soren@tech-logic.dk>
2021-10-04 17:32:26 +0200

    update cors

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -24,5 +24,6 @@ public class ApplicationConfig extends Application {
         resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
         resources.add(rest.RenameMeResource.class);
         resources.add(rest.PersonResource.class);
+        resources.add(rest.CORS.CORSFilter.class);
     }
 }
